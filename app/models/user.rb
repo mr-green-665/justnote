efb749ac21fb9767c909b947e74d0edcef3d873d
Roman Ventskus <scofield665@gmail.com>
2015-05-03 16:06:55 +0300

    Authentication fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ActiveRecord::Base
 	has_one :note
 
 	def self.from_omniauth(auth)
-		search_params = auth.permit(:provider, :uid)
-		where(search_params).first || create_from_omniauth(auth)
+		logger.debug("Params auth debug:")
+		logger.debug(auth.inspect)
+		where(:provider => auth["uid"], :uid => auth["provider"]).first || create_from_omniauth(auth)
 	end
 
 	def self.create_from_omniauth(auth)
