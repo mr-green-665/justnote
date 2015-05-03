cf99cd48af2955ed361945a862c5e7288d599801
Roman Ventskus <scofield665@gmail.com>
2015-05-03 15:48:13 +0300

    Signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,14 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.from_omniauth(env["omniauth.auth"])
+		logger.debug("dsfdsf")
+		logger.debug(user)
 		session[:user_id] = user.id
 		redirect_to root_url, notice: "Signed!"
+	end
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to root_url, notice: "Signed out!"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "notes#index"
   get "auth/:provider/callback", to: "sessions#create"
+  get 'signout', to: "sessions#destroy", as: "signout"
   resources :notes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
